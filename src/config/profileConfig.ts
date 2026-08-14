01a2adcdd1efc2cbbcbc5da4771a99d7ee785f55
wuan711 <1705905265@qq.com>
2026-08-14 11:03:04 +0800

    这是我的博客

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "午安123",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "喜欢睡午觉",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
