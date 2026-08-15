3557151dc6eba989f1ac35adbe6a310d83e76875
wuan711 <1705905265@qq.com>
2026-08-15 22:23:58 +0800

    更新名字2

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -18,7 +18,7 @@ export const galleryConfig: GalleryConfig = {
 		{
 			id: "firefly-2026",
 			name: "可爱",
-			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
+			description: "绽放在终竟的明天。",
 			location: "崩坏：星穹铁道",
 			date: "2026-01-01",
 			tags: ["崩道", "流萤"],
